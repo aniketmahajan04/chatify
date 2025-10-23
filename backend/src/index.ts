45267d345f99cf160ea1034af1d4328b13b47c90
mahajananiket04 <mahajananiket207@gmail.com>
2025-10-23 11:32:10 +0530

    initializing clerk authentication

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,35 @@
+import "dotenv/config";
 import express, { Request, Response } from "express";
 import { PORT } from "./config/config";
+import {
+  clerkClient,
+  requireAuth,
+  getAuth,
+  clerkMiddleware,
+} from "@clerk/express";
 
 const app = express();
 
 app.use(express.json());
+app.use(clerkMiddleware());
 
 app.get("/", (req: Request, res: Response) => {
-    res.send("Hello from server");
+  res.send("Hello from server");
+});
+// Use requireAuth() to protect this route
+// If user isn't authenticated, requireAuth() will redirect back to the homepage
+app.get("/protected", requireAuth(), async (req, res) => {
+  // Use `getAuth()` to get the user's `userId`
+  const auth = getAuth(req);
+  console.log(auth);
+
+  if (!auth.userId) return res.status(401).json({ error: "No user session" });
+  // Use Clerk's JavaScript Backend SDK to get the user's User object
+  const user = await clerkClient.users.getUser(auth.userId);
+
+  return res.json({ user });
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on ${PORT}`);
+  console.log(`Server is running on ${PORT}`);
 });
