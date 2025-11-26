80b191526464c3b41c729ad42cfc0a4a86722801
aniketmahajan04 <mahajananiket207@gmail.com>
2025-11-26 16:33:37 +0530

    implement sockets state

--- backend/src/websockets/socket.ts
+++ backend/src/websockets/socket.ts
@@ -1,6 +1,36 @@
 import { Server } from "socket.io";
 import { verifyToken } from "@clerk/backend";
 
+// Track: userId → Set of socketIds
+export const userSocket = new Map<string, Set<string>>();
+
+// Track: socketId → userId
+export const socketUser = new Map<string, string>();
+
+const addSockets = (userId: string, socketId: string) => {
+  if (!userSocket.has(userId)) {
+    userSocket.set(userId, new Set());
+  }
+
+  userSocket.get(userId)?.add(socketId);
+  socketUser.set(socketId, userId);
+}
+
+const removeSockets = (socketId: string) => {
+  const userId = socketUser.get(socketId);
+  if (!userId) return;
+
+  const sockets = userSocket.get(userId);
+  if (sockets) {
+    sockets.delete(socketId);
+    // if user has no sockets left, remove them from userSocket
+    if (sockets?.size === 0) {
+      userSocket.delete(userId);
+    }
+  }
+
+  socketUser.delete(socketId);
+}
 
 export function socketServer(io: Server) {
   io.use(async (socket, next) => {
@@ -26,6 +56,7 @@ export function socketServer(io: Server) {
         email: payload.email,
       }
 
+
       return next();
     } catch (err: any) {
       console.log("Socket auth failed: ", err);
@@ -35,11 +66,13 @@ export function socketServer(io: Server) {
   io.on("connection", (socket) => {
     const userId = socket.data.userId;
     console.log(`socket connected: ${socket.id} user=${userId}`);
+    addSockets(userId, socket.id);
 
 
 
     socket.on("disconnect", (reason) => {
       console.log(`socket disconnect: ${socket.id} user=${userId} reason=${reason}`)
+      removeSockets(socket.id);
     });
 
     socket.emit("connected", {
